fa352d1a34d7a20f575bc091769261dce5c171a1
sagarannaldas <annal7057@gmail.com>
2023-07-19 00:16:53 +0530

    1. Created ChooseLocationActivity.kt screen

---
+++ app/src/main/java/com/example/wavesoffood/ChooseLocationActivity.kt
@@ -0,0 +1,22 @@
+package com.example.wavesoffood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.wavesoffood.databinding.ActivityChooseLocationBinding
+
+class ChooseLocationActivity : AppCompatActivity() {
+    private val binding: ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val locationList = arrayOf("Mumbai", "Delhi", "Bangalore", "Hyderabad")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
+    }
+}
