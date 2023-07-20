edc7fcedda27ff72acf4c0bc37a9cb6abc64ba9a
sagarannaldas <annal7057@gmail.com>
2023-07-20 13:36:21 +0530

    1. Created ProfileFragment

--- app/src/main/java/com/example/wavesoffood/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragment/ProfileFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.wavesoffood.R
+import com.example.wavesoffood.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +18,9 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
